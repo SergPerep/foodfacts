59b1fb88ff00eb882023086e0cccde4625f20233
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-06-07 22:04:18 +0200

    Delete commented code

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import createHomePage from './pages/homePage.js';
 function loadApp() {
   const appRoot = document.getElementById('app-root');
   
-
-
-
   const { root } = createHomePage();
   appRoot.appendChild(root);
 }
